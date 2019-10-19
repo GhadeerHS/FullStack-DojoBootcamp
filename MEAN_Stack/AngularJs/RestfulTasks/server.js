a271c23d927a0c82fbb2bbaa4885793e4fd1b91d
Ghadeer Hijji <Ghadeer.hijji91@gmail.com>
2019-10-19 23:47:44 +0300

    complete Angular Task1

--- MEAN_Stack/AngularJs/RestfulTasks/server.js
+++ MEAN_Stack/AngularJs/RestfulTasks/server.js
@@ -1,70 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const ObjectId = require('mongoose').Types.ObjectId;
-
-const bodyParser = require('body-parser');
 const app = express();
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(9000, function () {
-    console.log("listening on port 9000");
-})
 app.use(express.static( __dirname + '/public/dist/public' ));
 
-mongoose.connect('mongodb://localhost/tasks', { useNewUrlParser: true });
-const tasksSchema = new mongoose.Schema({
-    title: { type: String, required: [true, 'Name cannot be empty'] },
-    description: { type: String, default: '' },
-    completed: { type: Boolean, default: false }
-}, { timestamps: true })
+require('./server/config/routes.js')(app);
+require('./server/config/mongoose.js');
 
-const Task = mongoose.model('Task', tasksSchema);
-
-
-app.get('/', (req, res) => {
-    res.render('index.html');
-    // Task.find({}, (err, docs) => {
-    //     res.json(docs)
-    // }).sort({createdAt: -1});
-})
-
-app.get('/:id', (req, res) => {
-    Task.findOne({_id: req.params.id }, (err, doc) => {
-        console.log(doc);
-        res.json(doc);
-    })
-})
-
-app.post('/create', (req, res) => {
-    Task.create(req.body, (err, doc) => {
-        if (err) {
-            console.log('Error in creating the document - ' + err);
-        } else {
-            res.json(doc);
-        }
-    })
-})
-
-app.put('/:id', (req, res) => {
-    let data = {};
-    for (let key in req.body) {
-        console.log('> '+key+' '+req.body[key]);
-        data[key] = req.body[key];
-    }
-    Task.updateOne({_id: ObjectId(req.params.id)}, {$set: data}, (err, doc) => {
-        if (err) {
-            console.log('Error in updating the document -> ' + req.params.id);
-        } else {
-            res.json(doc);
-        }
-    })
-})
-
-app.delete('/:id', (req, res) => {
-    Task.deleteOne({_id: req.params.id}, (err) => {
-        if (err) {
-            console.log('Error in deleting the document -> ' + req.params.id);
-        }
-    })
-})
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
