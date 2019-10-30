855c331b9631707f1bacc58fe91f8a62b2f4227d
Ghadeer Hijji <Ghadeer.hijji91@gmail.com>
2019-10-31 00:46:55 +0300

    adding Java assignments3

---
+++ Java_Stack/JavaFund/StringManipulation/StringManipulationTest.java
@@ -0,0 +1,30 @@
+public class StringManipulationTest {
+    public static void main(String[] args) {
+        StringManipulation concat = new StringManipulation();
+        String output = concat.trimAndConcat("   Hello    ", "   World   ");
+        System.out.println(output);
+        StringManipulation index = new StringManipulation();
+
+        char letter = 'o';
+        Integer a = index.getIndexOrNull("Coding", letter);
+        Integer b = index.getIndexOrNull("Hello World", letter);
+        Integer c = index.getIndexOrNull("Hi", letter);
+        System.out.println(a); // 1
+        System.out.println(b); // 4
+        System.out.println(c); // null
+
+        StringManipulation index2 = new StringManipulation();
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer d = index2.getIndexOrNull(word, subString);
+        Integer e = index2.getIndexOrNull(word, notSubString);
+        System.out.println(d); // 2
+        System.out.println(e); // null
+
+        StringManipulation concatSubstring = new StringManipulation();
+        String result = concatSubstring.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(result); // eworld
+
+    }
+}
