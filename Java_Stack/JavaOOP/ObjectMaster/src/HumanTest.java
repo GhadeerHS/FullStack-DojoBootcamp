327dc481009fb8686406ffbade64fbc9acf2d097
Ghadeer Hijji <Ghadeer.hijji91@gmail.com>
2019-11-05 00:47:41 +0300

    adding JavaOOP assignments4

---
+++ Java_Stack/JavaOOP/ObjectMaster/src/HumanTest.java
@@ -0,0 +1,19 @@
+public class HumanTest {
+
+	public static void main(String[] args) {
+
+		Human ghadeer = new Human();
+		Human ashwaq = new Human();
+		
+		ghadeer.attack(ashwaq);
+		ghadeer.attack(ashwaq);
+		ghadeer.attack(ashwaq);
+		ghadeer.attack(ashwaq);
+		ghadeer.attack(ashwaq);
+		ghadeer.attack(ashwaq);
+	
+		
+		
+	}
+
+}
