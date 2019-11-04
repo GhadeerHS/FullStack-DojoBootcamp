8ec9963531f3a55c7485c9f2ab52fa5aaa75dbb3
Ghadeer Hijji <Ghadeer.hijji91@gmail.com>
2019-11-05 01:11:35 +0300

    adding JavaOOP assignments6

---
+++ Java_Stack/JavaOOP/BankAccount/src/BankAccountTest.java
@@ -0,0 +1,20 @@
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+
+		BankAccount acct1 = new BankAccount();
+		BankAccount acct2 = new BankAccount();
+		acct1.getBalances();
+		acct1.depositMoney(100, "Checking");
+		acct1.depositMoney(500, "Savings");
+		acct1.getBalances();
+		acct2.getBalances();
+		acct2.depositMoney(200, "Checking");
+		acct2.depositMoney(700, "Savings");
+		acct2.getBalances();
+		acct1.withdrawMoney(50, "Checking");
+		acct1.getBalances();
+		BankAccount.getTotalBankMoney();
+	}
+
+}
