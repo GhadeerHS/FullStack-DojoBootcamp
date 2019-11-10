549e018d170a3640340baf6a2058f7517c466cf1
Ghadeer Hijji <Ghadeer.hijji91@gmail.com>
2019-11-10 21:49:29 +0300

    adding JavaSpring assignments5

---
+++ Java_Stack/Spring/TheCode/src/main/java/com/ghadeer/TheCode/HelloController.java
@@ -0,0 +1,33 @@
+package com.ghadeer.TheCode;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping(value="/submit", method=RequestMethod.POST)
+	public String submit(@RequestParam(value="code") String code, RedirectAttributes redirectAttributes) {
+		if(code.equals("bushido")) {
+			return "redirect:/code";
+		}
+		else {
+			redirectAttributes.addFlashAttribute("error","You must train harder!");
+			return "redirect:/";
+		}
+	}
+	
+	@RequestMapping("/code")
+	public String code() {
+		return "code.jsp";
+	}
+
+}
